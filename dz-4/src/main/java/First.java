5f5ac1b7ed6a8247dbf02db7a419d623837dbafe
Oleh <leonovolegm95@gmail.com>
2023-06-20 13:43:07 +0300

    dz-4

---
+++ dz-4/src/main/java/First.java
@@ -0,0 +1,9 @@
+public class First
+{
+    static public void main(String[] argv)
+    {
+        Second next = new Second();
+        int intNumber2 = next.getInt();
+        System.out.println("~~~<"+ intNumber2+ ">~~~");
+    }
+}
